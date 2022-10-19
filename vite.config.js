8591259fbc439ca5993a306934934b93a27d5d94
Diego Montivero <diego.montivero91@gmail.com>
2022-10-19 03:44:14 +0200

    Updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
@@ -7,5 +8,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: "0.0.0.0",
+  },
+  resolve: {
+    alias: {
+      "@components": resolve(__dirname, "src/components"),
+      "@services": resolve(__dirname, "src/services"),
+    },
   },
 });
